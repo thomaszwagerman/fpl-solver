1dcc7f39ac451937a8ef53a0de9312d39298f7d4
thomaszwagerman <thozwa@bas.ac.uk>
2025-08-01 21:02:22 +0100

    remove duplicate logger

--- fpl_solver/xp_predictor.py
+++ fpl_solver/xp_predictor.py
@@ -14,33 +14,9 @@ from datetime import datetime
 from typing import Dict, List, Optional, Union, Tuple
 import logging
 import sys
-# Local import instead of package import to avoid circular dependencies
+# Local imports
 from .cache_manager import CacheManager
-
-def setup_logger(name: str, level: Optional[int] = None) -> logging.Logger:
-    """
-    Set up a logger with consistent formatting and optional level override.
-    
-    Args:
-        name: The name of the logger
-        level: Optional logging level override. If None, uses INFO
-        
-    Returns:
-        logging.Logger: Configured logger instance
-    """
-    logger = logging.getLogger(name)
-    
-    if not logger.handlers:  # Only add handler if logger doesn't have one
-        handler = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-    
-    logger.setLevel(level or logging.INFO)
-    return logger
+from .logger import setup_logger
 
 # Import configurations from config file
 from .config import (
